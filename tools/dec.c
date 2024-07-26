f421ce15ee7aa7cb722d9a3fc4d1c2f6e89706bb
Jtorrellas22 <intrepidoshark@gmail.com>
2024-07-26 13:58:07 -0400

    added ceaser decryption

--- tools/dec.c
+++ tools/dec.c
@@ -5,6 +5,7 @@
 
 #define BUFFER_SIZE 1024
 #define AES_BLOCK_SIZE 16
+#define TEXT_LENGTH 84
 
 int decrypt_aes_cbc(const byte* encrypted_input, word32 input_len, 
                     const byte* key, const byte* iv, 
@@ -19,12 +20,40 @@ int decrypt_aes_cbc(const byte* encrypted_input, word32 input_len,
         return ret;
     }
 
-    // decrypt
+    // Decrypt
     ret = wc_AesCbcDecrypt(&aes, decrypted_output, encrypted_input, input_len);
     if (ret != 0) {
         printf("Error decrypting: %d\n", ret);
         return ret;
     }
 
-    return 0; //Success
+    return 0; // Success
+}
+
+int caesar_decrypt(char *ciphertext, int shift, char *decrypted) {
+    int i;
+    char ch;
+    // Iterate through characters
+    for (i = 0; i < TEXT_LENGTH; ++i) {
+        ch = ciphertext[i];
+        // Uppercase
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ((ch - 'A' - shift + 26) % 26) + 'A';
+            decrypted[i] = ch;
+        }
+        // Lowercase
+        else if (ch >= 'a' && ch <= 'z') {
+            ch = ((ch - 'a' - shift + 26) % 26) + 'a';
+            decrypted[i] = ch;
+        }
+        // Anything else
+        else {
+            decrypted[i] = ch;
+        }
+    }
+
+    return 0;
+
+    // does not include null terminator since we're assuming its all the same length
+    // decrypted[TEXT_LENGTH] = '\0';
 }
